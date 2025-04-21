44260de3e07a2de7d2a4c097a8dc24e220d22954
Raj-Ayush <rajayush8571@gmail.com>
2025-04-21 06:32:09 +0530

    fix(updated next.config.js file)

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['media-stream.b-cdn.net'],
     unoptimized: true
-  },
-  experimental: {
-    appDir: true,
-  },
-}
+  }
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
